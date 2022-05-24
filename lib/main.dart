142d29fbd19eb98c9ffaaae7d1eee075996200a5
albertobezerra <albertofbezerra@gmail.com>
2022-05-24 12:16:43 -0300

    Customização da estrutura base

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:path_provider/path_provider.dart';
 void main() {
   runApp(const MaterialApp(
     home: Home(),
+    debugShowCheckedModeBanner: false,
   ));
 }
 
@@ -21,7 +22,59 @@ class _HomeState extends State<Home> {
   List _toDoList = [];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tent.1 TodoLocal'),
+        centerTitle: true,
+        backgroundColor: Colors.amberAccent,
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 15, 10, 10),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            width: 3, color: Colors.amberAccent),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            width: 3, color: Colors.amberAccent),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      labelText: 'Digita logo, antes que esqueça!',
+                      labelStyle: TextStyle(
+                        color: Colors.amberAccent,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 8),
+                RaisedButton(
+                  padding: EdgeInsets.all(20),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  onPressed: () {},
+                  color: Colors.amberAccent,
+                  child: Text(
+                    '+',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 
   Future<File> _getFile() async {
