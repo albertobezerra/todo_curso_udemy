6462db957879844a33d753129dca79cbdc460b81
albertobezerra <albertofbezerra@gmail.com>
2022-05-25 11:40:34 -0300

    Ordenando a lista
    Não gostei pq tem q puxar. Seria bem melhor que após clicado na opção de feito ele já atualizasse automaticamente!

--- lib/main.dart
+++ lib/main.dart
@@ -47,6 +47,22 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {
+      _toDoList.sort((a, b) {
+        if (a['ok'] && !b['ok'])
+          return 1;
+        else if (!a['ok'] && b['ok'])
+          return -1;
+        else
+          return 0;
+      });
+      _saveData();
+    });
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -101,10 +117,13 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.only(top: 10),
-              itemCount: _toDoList.length,
-              itemBuilder: buildItem,
+            child: RefreshIndicator(
+              onRefresh: _refresh,
+              child: ListView.builder(
+                padding: EdgeInsets.only(top: 10),
+                itemCount: _toDoList.length,
+                itemBuilder: buildItem,
+              ),
             ),
           ),
         ],
@@ -147,7 +166,8 @@ class _HomeState extends State<Home> {
           _saveData();
 
           final snack = SnackBar(
-            content: Text('Terminasse isso \'${_lastRemoved['title']}\' mesmo?'),
+            content:
+                Text('Terminasse isso \'${_lastRemoved['title']}\' mesmo?'),
             action: SnackBarAction(
                 label: 'Terminei não',
                 onPressed: () {
